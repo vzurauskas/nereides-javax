3ab95bb1bd82382b47fe8fb609796bbecab00300
Vytautas Žurauskas <zurauskas.vytautas@gmail.com>
2019-11-01 20:39:52 +0200

    #11 - SmartJson.

---
+++ src/main/java/com/vzurauskas/nereides/javax/SmartJson.java
@@ -0,0 +1,185 @@
+package com.vzurauskas.nereides.javax;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Collections;
+import java.util.Optional;
+import javax.json.JsonObject;
+import javax.json.JsonStructure;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+
+/**
+ * A smart JSON. It can represent itself in other data types such as,
+ * byte arrays, {@link String}s, byte arrays and so forth. It can also
+ * give its nested JSONs and leaves, tell if it is missing and do other useful
+ * things. To use it, you need to wrap another {@link Json} in it, e.g.
+ * <pre>
+ * {@code
+ * Json original = new Json.Of(...);
+ * SmartJson smart = new SmartJson(original);
+ * String textual = smart.textual();
+ * InputStream stream = smart.inputStream();
+ * SmartJson nested = smart.at("/path/to/nested/json");
+ * if (!nested.isMissing()) {
+ *     Optional<String> value = nested.leaf("fieldName");
+ * }
+ * }
+ * </pre>
+ */
+public final class SmartJson implements Json {
+
+    private final Json origin;
+    private final Unchecked<JsonStructure> structure;
+
+    /**
+     * Constructor.
+     * @param origin Original JSON as basis to this {@code SmartJson}.
+     */
+    public SmartJson(Json origin) {
+        this(
+            origin,
+            new Unchecked<>(
+                () -> javax.json.Json.createReader(origin.bytes()).read()
+            )
+        );
+    }
+
+    private SmartJson(Json origin, Unchecked<JsonStructure> structure) {
+        this.origin = origin;
+        this.structure = structure;
+    }
+
+    @Override
+    public InputStream bytes() {
+        return origin.bytes();
+    }
+
+    /**
+     * Represent this JSON in textual form.
+     * @return String representing this JSON in textual form.
+     */
+    public String textual() {
+        return new Unchecked<>(
+            () -> {
+                try (Writer writer = new StringWriter()) {
+                    javax.json.Json.createWriter(writer).write(
+                        structure.value()
+                    );
+                    return writer.toString();
+                }
+            }
+        ).value();
+    }
+
+    /**
+     * Represent this JSON in pretty format textual form.
+     * @return String representing this JSON in pretty format textual form.
+     */
+    public String pretty() {
+        return new Unchecked<>(
+            () -> {
+                try (Writer writer = new StringWriter()) {
+                    javax.json.Json.createWriterFactory(
+                        Collections.singletonMap(
+                            JsonGenerator.PRETTY_PRINTING, true
+                        )
+                    ).createWriter(writer).write(structure.value());
+                    return writer.toString();
+                }
+            }
+        ).value();
+    }
+
+    /**
+     * Represent this JSON in an array of bytes.
+     * @return Byte array representing this JSON.
+     */
+    public byte[] byteArray() {
+        return new ByteArray(bytes()).value();
+    }
+
+    /**
+     * Method to get a {@code String} type field of this JSON.
+     * @param name Name of the field to return.
+     * @return Optional value of the field.
+     */
+    public Optional<String> leaf(String name) {
+        return Optional.of(structure.value().asJsonObject())
+            .filter(
+                o -> o.containsKey(name)
+                    && o.get(name).getValueType() == JsonValue.ValueType.STRING
+            )
+            .map(o -> o.getString(name));
+    }
+
+    /**
+     * Method to get an {@code int} type field of this JSON.
+     * @param name Name of the field to return.
+     * @return Optional value of the field.
+     */
+    public Optional<Integer> leafAsInt(String name) {
+        JsonObject obj = structure.value().asJsonObject();
+        return obj.containsKey(name)
+            ? Optional.of(obj.getInt(name, 0))
+            : Optional.empty();
+    }
+
+    /**
+     * Method to get a {@code double} type field of this JSON.
+     * @param name Name of the field to return.
+     * @return Optional value of the field.
+     */
+    public Optional<Double> leafAsDouble(String name) {
+        return Optional.of(structure.value().asJsonObject())
+            .filter(o -> o.containsKey(name))
+            .filter(
+                o -> o.get(name).getValueType() == JsonValue.ValueType.NUMBER
+            )
+            .map(o -> o.getJsonNumber(name).doubleValue());
+    }
+
+    /**
+     * Method to get a {@code boolean} type field of this JSON.
+     * @param name Name of the field to return.
+     * @return Optional value of the field.
+     */
+    public Optional<Boolean> leafAsBool(String name) {
+        JsonObject obj = structure.value().asJsonObject();
+        return obj.containsKey(name)
+            ? Optional.of(obj.getBoolean(name, false))
+            : Optional.empty();
+    }
+
+    /**
+     * Represent this JSON as {@link JsonStructure} in case full JSON
+     * manipulation capabilities offered by jackson-databind library are needed.
+     * @return This JSON as {@link JsonStructure}.
+     */
+    public JsonStructure jsonStructure() {
+        return structure.value();
+    }
+
+    /**
+     * Method to get a JSON nested within this JSON, specified by path. Path
+     * starts with a forward slash, and path elements are separated by forward
+     * slashes also, e.g.
+     * <pre>
+     * {@code
+     * SmartJson nested = json.at("/path/to/nested/json");}
+     * </pre>
+     * This method never returns null. If there is no JSON as specified by the
+     * path, a missing JSON is returned, i.e.
+     * {@code returned.isMissing() == true}.
+     * @param path Path to the nested JSON.
+     * @return The nested JSON, which could be missing.
+     */
+    public SmartJson at(String path) {
+        return new SmartJson(
+            new Of(
+                structure.value().asJsonObject().getValue(path).asJsonObject()
+            )
+        );
+    }
+}
---
+++ src/test/java/com/vzurauskas/nereides/javax/SmartJsonTest.java
@@ -0,0 +1,338 @@
+package com.vzurauskas.nereides.javax;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayOutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.json.JsonStructure;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+final class SmartJsonTest {
+
+    private final Path deep;
+
+    SmartJsonTest() throws URISyntaxException {
+        this.deep = Paths.get(
+            SmartJsonTest.class.getClassLoader()
+                .getResource("deep.json").toURI()
+        );
+    }
+
+    @Test
+    void givesByteStream() {
+        byte[] bytes = "{\"field1\":\"value1\",\"field2\":\"value2\"}"
+            .getBytes();
+        assertArrayEquals(
+            bytes,
+            new ByteArray(
+                new SmartJson(
+                    new Json.Of(bytes)
+                )
+            ).value()
+        );
+    }
+
+    @Test
+    void convertsToString() {
+        assertEquals(
+            "{\"field1\":\"value1\","
+                + "\"field2\":\"value2\"}",
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("field1", "value1")
+                        .add("field2", "value2")
+                        .build()
+                )
+            ).textual()
+        );
+    }
+
+    @Test
+    void convertsToPrettyString() {
+        assertEquals(
+            "\n{\n"
+                + "    \"field1\": \"value1\",\n"
+                + "    \"field2\": \"value2\"\n"
+                + '}',
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("field1", "value1")
+                        .add("field2", "value2")
+                        .build()
+                )
+            ).pretty()
+        );
+    }
+
+    @Test
+    void convertsToByteArray() {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        javax.json.Json.createWriter(stream).write(
+            javax.json.Json.createObjectBuilder()
+                .add("field1", "value1")
+                .add("field2", "value2")
+                .build()
+        );
+        byte[] bytes = stream.toByteArray();
+        assertArrayEquals(
+            bytes,
+            new SmartJson(new Json.Of(bytes)).byteArray()
+        );
+    }
+
+    @Test
+    void convertsToObjectNode() {
+        JsonStructure struct = javax.json.Json.createObjectBuilder()
+            .add("field1", "value1")
+            .add("field2", "value2")
+            .build();
+        assertEquals(
+            struct,
+            new SmartJson(
+                new Json.Of(struct)
+            ).jsonStructure()
+        );
+    }
+
+    @Test
+    void findsLeaf() {
+        assertEquals(
+            "value2",
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("field1", "value1")
+                        .add("field2", "value2")
+                        .build()
+                )
+            ).leaf("field2").get()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalForNonexistentLeaf() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder().build()
+                )
+            ).leaf("nonexistent").isPresent()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalIfLeafIsNotString() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("intField", 5)
+                        .build()
+                )
+            ).leaf("intField").isPresent()
+        );
+    }
+
+    @Test
+    void findsLeafAsInt() {
+        assertEquals(
+            14,
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("intField", 14)
+                        .build()
+                )
+            ).leafAsInt("intField").get().intValue()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalForNonexistentIntLeaf() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder().build()
+                )
+            ).leafAsInt("nonexistent").isPresent()
+        );
+    }
+
+    @Test
+    void returnsZeroIfLeafIsNotInt() {
+        assertEquals(
+            0,
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("intField", 5)
+                        .build()
+                )
+            ).leafAsInt("stringField").get().intValue()
+        );
+    }
+
+    @Test
+    void returnsIntEvenIfLeafIsDouble() {
+        assertEquals(
+            5,
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("doubleField", 5.9)
+                        .build()
+                )
+            ).leafAsInt("doubleField").get().intValue()
+        );
+    }
+
+    @Test
+    void findsLeafAsDouble() {
+        assertEquals(
+            14.9,
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("doubleField", 14.9)
+                        .build()
+                )
+            ).leafAsDouble("doubleField").get().doubleValue()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalForNonexistentDoubleLeaf() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder().build()
+                )
+            ).leafAsDouble("nonexistent").isPresent()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalIfLeafIsNotDouble() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("intField", 14)
+                        .build()
+                )
+            ).leafAsDouble("stringField").isPresent()
+        );
+    }
+
+    @Test
+    void returnsDoubleEvenIfLeafIsInt() {
+        assertEquals(
+            5.0,
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("intField", 5)
+                        .build()
+                )
+            ).leafAsDouble("intField").get().doubleValue()
+        );
+    }
+
+    @Test
+    void findsLeafAsBool() {
+        assertTrue(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("boolField", true)
+                        .build()
+                )
+            ).leafAsBool("boolField").get()
+        );
+    }
+
+    @Test
+    void returnsEmptyOptionalForNonexistentBooleanLeaf() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder().build()
+                )
+            ).leafAsBool("nonexistent").isPresent()
+        );
+    }
+
+    @Test
+    void returnsFalseIfLeafIsNotBool() {
+        assertFalse(
+            new SmartJson(
+                new Json.Of(
+                    javax.json.Json.createObjectBuilder()
+                        .add("stringField", "stringValue")
+                        .add("boolField", true)
+                        .build()
+                )
+            ).leafAsBool("stringField").get()
+        );
+    }
+
+    @Test
+    void findsPath() {
+        assertEquals(
+            "red",
+            new SmartJson(
+                new Json.Of(deep)
+            ).at("/ocean/rock1/nereid2").leaf("hair").get()
+        );
+    }
+
+    @Test
+    void findsPathInArray() {
+        assertEquals(
+            "Jason",
+            new SmartJson(
+                new Json.Of(deep)
+            ).at("/ocean/rock1/nereid1/associates/0").leaf("name").get()
+        );
+    }
+
+    @Disabled("https://github.com/vzurauskas/nereides-javax/issues/26")
+    @Test
+    void understandsArrays() {
+        String array = "[{\"name\":\"Jason\"},{\"name\":\"Thetis\"}]";
+        assertArrayEquals(
+            array.getBytes(),
+            new ByteArray(
+                new SmartJson(
+                    new Json.Of(deep)
+                ).at("/ocean/rock1/nereid1/associates")
+            ).value()
+        );
+    }
+
+    @Disabled("https://github.com/vzurauskas/nereides-javax/issues/26")
+    @Test
+    void reallyUnderstandsArrays() {
+        assertEquals(
+            "Jason",
+            new SmartJson(
+                new Json.Of(deep)
+            ).at("/ocean/rock1/nereid1/associates").at("/0").leaf("name").get()
+        );
+    }
+}
